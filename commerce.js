b7062f3d0b142094e0fc3df467eb483e2e3138c3
Oke-Precious <okepreciousab@gmail.com>
2023-02-28 15:12:03 +0100

    added meganav

--- commerce.js
+++ commerce.js
@@ -9,8 +9,71 @@ const landingPageScroll=()=>{
     else{
         
     }
+    
 }
 const cancelHeader=()=>{
     navHeader.innerHTML="";
     navHeader.style.padding="0px";
+}
+
+const dropObject=(param)=>{
+    if(param==1){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>I'm Category</h1>
+                    `
+    }
+
+    else if(param==2){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Computer Accessories</h1>
+                    `
+    }
+    else if(param==3){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Phones and Tables</h1>
+                        `
+    }
+    else if(param==4){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Electronics</h1>
+                    `
+    }
+    else if(param==5){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Special Fashion</h1>
+                    `
+    }
+    else if(param==6){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Home and Kitchen</h1>
+                    `
+    }
+    else if(param==7){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Baby, Kids and Toys</h1>
+                    `
+    }
+    else if(param==7){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Baby, Kids and Toys</h1>
+                    `
+    }
+    else if(param==8){
+        dropContainer.style.display="block";
+        dropContainer.innerHTML=`
+                        <h1>Other Categories</h1>
+                    `
+    }
+    else{
+        dropContainer.style.display="none"
+    }
+
 }
